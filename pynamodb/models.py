c7d46db4aa03af371c719dbd4878e7dd739b550a
Aurélien Ballier <aurelien.ballier@cyclonit.com>
2015-07-23 19:29:06 +0200

    Remove connection caching

--- pynamodb/models.py
+++ pynamodb/models.py
@@ -1108,16 +1108,14 @@ class Model(with_metaclass(MetaModel)):
     @classmethod
     def _get_connection(cls):
         """
-        Returns a (cached) connection
+        Returns a (non cached) connection
         """
         if not hasattr(cls, "Meta") or cls.Meta.table_name() is None:
             raise AttributeError(
                 """As of v1.0 PynamoDB Models require a `Meta` class.
                 See http://pynamodb.readthedocs.org/en/latest/release_notes.html"""
             )
-        if cls._connection is None:
-            cls._connection = TableConnection(cls.Meta.table_name(), region=cls.Meta.region, host=cls.Meta.host)
-        return cls._connection
+        return TableConnection(cls.Meta.table_name(), region=cls.Meta.region, host=cls.Meta.host)
 
     def _deserialize(self, attrs):
         """
